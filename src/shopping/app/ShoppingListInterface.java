7c7caf8bfa45b3dab491807156d24515ac6d3a8e
groiq <35578027+groiq@users.noreply.github.com>
2018-06-13 12:41:53 +0200

    Create new node method implemented

--- src/shopping/app/ShoppingListInterface.java
+++ src/shopping/app/ShoppingListInterface.java
@@ -8,6 +8,8 @@ import shopping.list.Unit;
 
 public class ShoppingListInterface {
 	
+	final static ShoppingList groceryList = new ShoppingList();
+	
 	public static void main(String[] args) {
 		
 		Out.println("Shopping list App");
@@ -15,7 +17,7 @@ public class ShoppingListInterface {
 		Out.println();
 		
 		// shopping list object
-		final ShoppingList groceryList = new ShoppingList();
+//		groceryList = new ShoppingList();
 		
 		// Read test data from GroceryInit.txt
 		In.open("GroceryInit.txt");
@@ -57,8 +59,8 @@ public class ShoppingListInterface {
 		Out.println("3: Print a selection of items");
 		Out.println("4: Print a single item (by ID");
 		separate();
-		Out.println("4: Delete an item (by ID)");
-		Out.println("5: Delete all items of one product group");
+		Out.println("5: Delete an item (by ID)");
+		Out.println("6: Delete all items of one product group");
 		separate();
 		Out.println("h: show this list");
 		Out.println("q: quit");
@@ -66,19 +68,60 @@ public class ShoppingListInterface {
 	}
 	
 	private static void interactionCycle() {
-		Out.println("Enter one of (1,2,3,4,5,h,q): ");
+		Out.print("Enter one of (1,2,3,4,5,6,h,q): ");
 		char selection = In.readChar();
 //		char selection = parseInput();
+		separate();
 		if (selection != 'q') {
 			switch (selection) {
-			case '1':
+			case '1':	// create a new item
 				Out.println("adding a new item...");
+				Out.println("--------------------");
+				Out.print("enter description: ");
+				String description = In.readWord();
+//				In.readLine();
+//				Out.println(description);
+				Out.println("Available product groups:");
+				ProductGroup[] pgValues = ProductGroup.values();
+				for (ProductGroup value : pgValues) {
+					Out.println(value.ordinal() + ": " + value);
+				}
+				Out.print("Enter number of selected product group: ");
+				int pgNum = In.readInt();
+				ProductGroup productGroup = pgValues[pgNum];
+				Out.println("Available units:");
+				Unit[] unitValues = Unit.values();
+				for (Unit value : unitValues) {
+					Out.println(value.ordinal() + ": " + value);
+				}
+				Out.print("Enter number of selected unit: ");
+				int unitNum = In.readInt();
+				Unit unit = unitValues[unitNum];
+				Out.print("Enter quantity (as integer): ");
+				int quantity = In.readInt();
+				groceryList.newItem(description, unit, quantity, productGroup);
+				Out.println("Created entry: " + quantity + " " + unit + " of " + description + " needed from " + productGroup + ".");
 				break;
-			case '2':
+			case '2':	// print all
+				Out.println("all items:");
+				Out.println("----------");
+				printList(groceryList.getAll());
+				break;
+			case '3':	// print selected
+				break;
+			case '4':	// print single
+				break;
+			case '5':	// delete single 
+				break;
+			case '6':	// delete by group
+				break;
+			case 'h':	// print menu
+				printAvailable();
 				break;
 			default:
 				Out.print("Error: not a valid selection. ");
 			}
+			separate();
 			interactionCycle();
 		} else {
 			Out.println("Exiting...");
@@ -89,6 +132,16 @@ public class ShoppingListInterface {
 		Out.println("Enter one of (1,2,3,4,5,h,q): ");
 		char selection = In.readChar();
 		return selection;
+	}
+	
+	private static void printList(String[] entries) {
+		if (entries.length == 0) {
+			Out.println("No items found.");
+		} else {
+			for (String entry : entries) {
+				Out.println(entry);
+			}
+		}
 	}
 	
 }
